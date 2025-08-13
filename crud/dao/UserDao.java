3b0caf53de5c399a25c27b8362315c7d8a47d724
samarthyasurve17 <samarthyasurve17@gmail.com>
2025-08-13 22:29:04 +0530

    Delete crud directory

--- crud/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.samarthyasApplication.crud.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.samarthyasApplication.crud.model.User;
-
-public interface UserDao extends JpaRepository<User, Long>{
-
-}
