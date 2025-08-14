1a1d08216eeaaf42015feeadb951c665743961b8
samarthyasurve17 <samarthyasurve17@gmail.com>
2025-08-15 01:29:29 +0530

    Delete src directory

--- src/components/Curdapp.jsx
+++
@@ -1,9 +0,0 @@
-const Curdapp = () => {
-  return (
-    <div className="curdapp">
-      <h1>CRUD Application</h1>
-      <p>This is a simple CRUD application.</p>
-    </div>
-  );
-}
-export default Curdapp;
